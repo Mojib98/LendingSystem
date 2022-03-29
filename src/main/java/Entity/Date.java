578f89866569d3828ca07f890f77f5a80df15385
arkad <mojib.khabireh@gmail.com>
2022-03-29 10:49:17 +0430

    date class create

--- src/main/java/Entity/Date.java
+++ src/main/java/Entity/Date.java
@@ -3,11 +3,28 @@ package Entity;
 import com.ghasemkiani.util.icu.PersianCalendar;
 import com.ibm.icu.util.Calendar;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Transient;
+
+@Embeddable
 public class Date {
+    @Column
+    java.sql.Date date;
+    @Transient
     PersianCalendar persianCalendar;
     public Date(int year, int month, int day) {
         persianCalendar = new PersianCalendar(year,month,day);
-
+        date = new java.sql.Date(persianCalendar.get(Calendar.YEAR),
+                persianCalendar.get(Calendar.MONTH),
+                persianCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
+    public java.sql.Date getDate() {
+        return this.date;
+    }
+
+    public Date() {
+
+    }
 }
